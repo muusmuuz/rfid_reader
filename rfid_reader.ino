6d7f67b53116164b6e360f40f2ff0c85ad9a1c29
Mosa <sellomosa@gmail.com>
2018-06-11 16:28:29 +0200

    rfid reader that returns individual tag IDs

---
+++ rfid_reader.ino
@@ -0,0 +1,60 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+constexpr uint8_t RST_PIN = 9;     // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = 10;     // Configurable, see typical pin layout above
+ 
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+MFRC522::MIFARE_Key key; 
+
+// Init array that will store new NUID 
+byte nuidPICC[4];
+
+void setup() {
+   Serial.begin(9600);
+  SPI.begin(); // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522 
+
+}
+
+void loop() {
+  // Look for new cards
+  if ( ! rfid.PICC_IsNewCardPresent()){
+    return;
+  }
+  
+   // Verify if the NUID has been read
+  if ( ! rfid.PICC_ReadCardSerial()) {
+     return;
+  }
+ MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+    // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return
+  }
+
+   if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+    rfid.uid.uidByte[1] != nuidPICC[1] || 
+    rfid.uid.uidByte[2] != nuidPICC[2] || 
+    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+    //Serial.println(F("A new card has been detected."));
+
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+     printHex(rfid.uid.uidByte, rfid.uid.size);
+
+    }
+}
+
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+    Serial.println();
+}
